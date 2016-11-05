ca8c8b26932638d317568c7bc4e77fa6405151dd
jamesj4318 <jamesj4318@gmail.com>
2016-11-05 13:22:09 +0900

    PushNewClass.java modified for git pull

--- Hello World/src/PushNewClass.java
+++ Hello World/src/PushNewClass.java
@@ -5,5 +5,6 @@ public class PushNewClass {
 		System.out.println("add and push new java class");
 		System.out.println("modify on remote repo.");
 		System.out.println("modify on local repo.");
+		System.out.println("modified on remote repo. for git pull");
 	}
 }
